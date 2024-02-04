57d046c67f4a4b15e4f77fa992003559a98029e7
Svetlozar Georgiev <svetlozar.georgiev@evnisoft.com>
2024-02-04 17:50:25 +0200

    Arthouse

--- dist/models/note.model.js
+++ dist/models/note.model.js
@@ -1,7 +1,16 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const mongoose_1 = require("mongoose");
-const noteSchema = new mongoose_1.Schema({}, { timestamps: true });
+const noteSchema = new mongoose_1.Schema({
+    client_id: {
+        type: String,
+        required: true,
+    },
+    title: {
+        type: String,
+        required: true,
+    },
+}, { timestamps: true });
 const Note = mongoose_1.models.Note || (0, mongoose_1.model)("Note", noteSchema);
 exports.default = Note;
 //# sourceMappingURL=note.model.js.map
--- src/models/note.model.ts
+++ src/models/note.model.ts
@@ -8,7 +8,19 @@ export interface INote extends Document {
   updatedAt: Date;
 }
 
-const noteSchema = new Schema({}, { timestamps: true });
+const noteSchema = new Schema(
+  {
+    client_id: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
 
 const Note = models.Note || model<INote>("Note", noteSchema);
 export default Note;
